bb2e087e948730a438dfcda330e10a51fb4dd587
Raymond Xu <53128754+AwesomestCode@users.noreply.github.com>
2021-03-27 20:25:41 -0400

    load jokes from resources folder

--- src/main/java/io/github/awesomestcode/futurehacks/factualquery/JokeResolver.java
+++ src/main/java/io/github/awesomestcode/futurehacks/factualquery/JokeResolver.java
@@ -4,6 +4,14 @@ import io.github.awesomestcode.futurehacks.BaseAnswerResolver;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class JokeResolver implements BaseAnswerResolver {
@@ -17,13 +25,20 @@ public class JokeResolver implements BaseAnswerResolver {
 
     public String resolve(String query) {
         FileReader fin = null;
+
+        URL url = this.getClass().getClassLoader().getResource("jokes.txt");
+        Path path = null;
+        List<String> lines;
         try {
-            fin = new FileReader("jokes.txt");
-        } catch (FileNotFoundException e) {
+            final Map<String, String> env = new HashMap<>();
+            final String[] array = url.toString().split("!");
+            final FileSystem fs = FileSystems.newFileSystem(URI.create(array[0]), env);
+            path = fs.getPath(array[1]);
+            lines = Files.readAllLines(path, StandardCharsets.UTF_8);
+        } catch (Exception e) {
             e.printStackTrace();
-            return "Something went wrong. Please try again";
+            return "Uh oh. An unknown error occurred. Please try again.";
         }
-        Scanner src = new Scanner(fin);
 
         double random = Math.random() * 21;
         double num = Math.floor(random);
@@ -33,12 +48,6 @@ public class JokeResolver implements BaseAnswerResolver {
         }
 
         int a = 0;
-        String line = "";
-
-        while (a != num) {
-            line = src.nextLine();
-            a++;
-        }
-        return line;
+        return lines.get((int) num);
     }
 }
